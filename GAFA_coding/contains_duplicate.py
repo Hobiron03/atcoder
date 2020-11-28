3b785569bc380dd2b30fb907966e7521a7298c1b
HOBIRON <yudainepnep@icloud.com>
2020-11-28 18:08:06 +0900

    ADD contains_duplicate.py

---
+++ GAFA_coding/contains_duplicate.py
@@ -0,0 +1,14 @@
+def has_duplicate(array: list) -> bool:
+    elements = {}
+    for el in array:
+        if el in elements:
+            elements[el] = True
+            return True
+        else:
+            elements[el] = False
+
+    return False
+
+
+if __name__ == "__main__":
+    print(has_duplicate([1, 2, 3, 4, 5]))
