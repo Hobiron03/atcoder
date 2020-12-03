7994f67e4f5af369f111d60943f0b1bbe864a3e3
HOBIRON <yudainepnep@icloud.com>
2020-12-04 01:28:26 +0900

    Add reverse_string.py

---
+++ GAFA_coding/reverse_string.py
@@ -0,0 +1,17 @@
+def reverse_string(string: str) -> str:
+    str_array = list(string)
+    start = 0
+    end = len(string) - 1
+    while start != end:
+        temp = str_array[start]
+        str_array[start] = str_array[end]
+        str_array[end] = temp
+
+        start += 1
+        end -= 1
+
+    return "".join(str_array)
+
+
+if __name__ == "__main__":
+    print(reverse_string("Hello,world"))
