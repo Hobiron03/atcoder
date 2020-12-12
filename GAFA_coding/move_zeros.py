ae8cfb81dad5b20438181a588bf3789cf9bd5a97
HOBIRON <yudainepnep@icloud.com>
2020-12-12 23:50:49 +0900

    refactor move_zero.py

--- GAFA_coding/move_zeros.py
+++ GAFA_coding/move_zeros.py
@@ -1,21 +1,17 @@
 def move_zeros(nums: list) -> list:
     runnerIndex = 0
-    nonZeroIndex = 0
+    zeroStartIndex = 0
 
     while runnerIndex < len(nums):
         if nums[runnerIndex] != 0:
             temp = nums[runnerIndex]
-            nums[runnerIndex] = nums[nonZeroIndex]
-            nums[nonZeroIndex] = temp
-            nonZeroIndex += 1
+            nums[runnerIndex] = nums[zeroStartIndex]
+            nums[zeroStartIndex] = temp
+            zeroStartIndex += 1
 
         runnerIndex += 1
 
     return nums
-
-
-def swap(a, b):
-    pass
 
 
 if __name__ == "__main__":
